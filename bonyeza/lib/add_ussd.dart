46cfafef960c9d8d90f49433bb7a15ef7dd7ff7e
ngash <ngash@localhost.localdomain>
2020-06-28 22:42:06 +0300

    added toggle button for the add_ussd page with some styling

--- bonyeza/lib/add_ussd.dart
+++ bonyeza/lib/add_ussd.dart
@@ -4,8 +4,8 @@ import 'package:flutter/painting.dart';
 import 'package:provider/provider.dart';
 import 'datase_helper.dart';
 import 'ussd_list_class.dart';
-
-
+import 'constants.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 String newDialNumber;
 String newDialName;
@@ -21,6 +21,8 @@ class AddUssd extends StatefulWidget {
 }
 
 class _AddUssdState extends State<AddUssd> {
+  List<bool> _isSelected = [false,false,false,false];
+  TextStyle toggleButtonsStyle =  TextStyle(fontWeight: FontWeight.w500);  //TODO use googleFonts
   TextEditingController controllerDialNumber;
   TextEditingController controllerDialName;
   AppDatabase database;
@@ -35,10 +37,11 @@ class _AddUssdState extends State<AddUssd> {
 
   @override
   Widget build(BuildContext context) {
+
     return
        Container(
       height: 300.0,
-        padding: EdgeInsets.all(15.0),
+        padding: EdgeInsets.all(11.0),
 
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -75,21 +78,61 @@ class _AddUssdState extends State<AddUssd> {
                 newDialName = newValue;
               },
             ),
-            RaisedButton(
-              elevation: 17.0,
-                color: Colors.indigoAccent,
-                onPressed:()async{
-//                  Ussd ussd =Ussd(id: null, dialNumber: newDialNumber,dialName: newDialName);
-//                  await database.insertUssd(ussd);
 
+            ToggleButtons(
+                children: <Widget>[
+                  Text('Safaricom',style:toggleButtonsStyle),
+                  Text('Airtel',style: toggleButtonsStyle,),
+                  Text('Telkom',style: toggleButtonsStyle,),
+                  Text('Banks',style: toggleButtonsStyle,)
+                ],
+                onPressed: (int index){
+                   setState(() {
+                     _isSelected[index] = !_isSelected[index];
+                   });
+                },
+                color: Colors.black,
+                splashColor: Colors.lightGreen,
+                borderColor: Colors.blueGrey,
+              borderRadius: BorderRadius.circular(15.0),
+                selectedBorderColor: Colors.black12,
+                selectedColor: Color(0xff080848),
+                fillColor: Colors.blueGrey,
+                isSelected:_isSelected ,
+            ),
+            RaisedButton(
+                elevation: 17.0,
+                color: Colors.black87,
+//            color: Color(0xff02020D),
+                hoverColor: Colors.indigo,
+                onPressed:()async{
+                  SafaricomUssd safaricomUssd =SafaricomUssd(id: null, dialNumber: newDialNumber,dialName: newDialName);
+                  AirtelUssd airtelUssd = AirtelUssd(id: null,dialNumber: newDialNumber,dialName: newDialName);
+
+                  if(_isSelected[0 ]== true){
+                    database.insertSafaricomUssd(safaricomUssd);
+                  }
+                  if(_isSelected[1 ]== true){
+                    database.insertAirtelUssd(airtelUssd);
+                  }
+                  if(_isSelected[2 ]== true){
+                    print('add to telkom');
+
+                  }
+                  if(_isSelected[3]== true){
+                    print('add to banks');
+
+                  }
                   resetValuesAfterSubmit();
                   Navigator.pop(context);
                 },
-                child: Text('Add',style: TextStyle(fontSize: 18.0),))
+                child: Text('Add',style: TextStyle(fontSize: 18.0,color: Colors.white),)),
           ],
         ),
     );
+
   }
+
 
   void resetValuesAfterSubmit(){
     setState(() {
